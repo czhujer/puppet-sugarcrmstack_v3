7abaa1867f2a6d0ed919abc7c14953669294783c
Patrik Majer <patrik.majer@atlas.cz>
2018-12-29 19:06:58 +0100

    add init class

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# Class: sugarcrmstack
+# ===========================
+#
+# Full description of class sugarcrmstack here.
+#
+# Parameters
+# ----------
+#
+# * `sample parameter`
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+class sugarcrmstack (){
+
+}
