16d05cd1487fed203b9ea3e8d5013b570854949e
Patrik Majer <patrik.majer@atlas.cz>
2018-12-29 19:19:47 +0100

    add basic code for init

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,21 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
-class sugarcrmstack (){
+class sugarcrmstack (
+  $sugar_version = '7.5'
+  ){
+
+  if defined(Class['sugarcrmstack_ng']){
+    if ($::sugarcrmstack_ng::sugar_version != '7.5' and $::sugarcrmstack_ng::sugar_version != '7.9' and $::sugarcrmstack_ng::sugar_version != '8.0'){
+      fail("Class['sugarcrmstack_ng::apachephpng']: This class is compatible only with sugar_version 7.5,7.9 or 8.0 (not ${::sugarcrmstack_ng::sugar_version})")
+    }
+  }
+  else{
+    if ($sugarcrmstack::sugar_version != '7.5' and $sugarcrmstack::sugar_version != '7.9'){
+      fail("Class['sugarcrmstack::apachephpng']: This class is compatible only with sugar_version 7.5 or 7.9 (not ${sugarcrmstack::sugar_version})")
+    }
+  }
+
+  notice "This class does nothing.. Use sugarcrmstack_ng or call sub-classes directly..."
 
 }
