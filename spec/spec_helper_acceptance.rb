f08d760d704048f73123c637165dbdddc1c2ab89
Patrik Majer <patrik.majer@atlas.cz>
2018-12-29 23:25:11 +0100

    fix modules install

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -42,15 +42,16 @@ RSpec.configure do |c|
     # Install module and dependencies
     # puppet_module_install(:source => proj_root, :module_name => 'sugarcrmstack')
     hosts.each do |host|
+      on host, puppet('module', 'install', 'puppetlabs-concat', '-v', '4.1.1'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-stdlib', '-v', '4.25.1'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-inifile', '-v', '2.4.0'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-apache', '-v', '3.4.0'), acceptable_exit_codes: [0, 1]
-      on host, puppet('module', 'install', 'puppetlabs-concat', '-v', '4.1.1'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-mysql', '-v', '6.2.0'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppet-cron', '-v', '1.3.1'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'thias-php', '-v', '1.2.2'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-firewall', '-v', '1.14.0'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-vcsrepo', '-v', '1.3.2'), acceptable_exit_codes: [0, 1]
+      on host, puppet('module', 'install', 'puppetlabs-git', '-v', '0.5.0'), acceptable_exit_codes: [0, 1]
       on(host, 'cd /etc/puppetlabs/code/modules && git clone https://github.com/SugarFactory/puppet-composer.git composer')
     end
   end
