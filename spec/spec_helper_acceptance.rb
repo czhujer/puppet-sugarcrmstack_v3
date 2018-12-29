876b262b8779265e1b8d3b9a23fbe83118a176fa
Patrik Majer <patrik.majer@atlas.cz>
2018-12-29 21:05:54 +0100

    switch module installation to old way for acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,21 +12,38 @@ end
 install_module
 
 # debug module status
-hosts.each do |host|
-  on(host, "ls -lh /etc/puppetlabs/code/modules/sugarcrmstack/*")
-end
+#hosts.each do |host|
+#  on(host, "ls -lh /etc/puppetlabs/code/modules/sugarcrmstack/*")
+#end
 
-install_module_dependencies
+#install_module_dependencies
 
 RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
   # Readable test descriptions
   c.formatter = :documentation
+
+  # some fixes
   hosts.each do |host|
     if host[:platform] =~ %r{el-6-x86_64} && host[:hypervisor] =~ %r{docker}
       on(host, "sed -i '/nodocs/d' /etc/yum.conf")
     end
-    if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
-      on(host, "sed -i '/nodocs/d' /etc/yum.conf")
+    #if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
+    #  on(host, "sed -i '/nodocs/d' /etc/yum.conf")
+    #end
+  end
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module and dependencies
+    #puppet_module_install(:source => proj_root, :module_name => 'sugarcrmstack')
+    hosts.each do |host|
+      on host, puppet('module', 'install', 'puppetlabs-stdlib', '-v', '4.25.1'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-inifile', '-v', '2.4.0'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-apache'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
     end
   end
 end
