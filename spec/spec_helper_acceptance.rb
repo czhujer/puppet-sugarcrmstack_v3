64c76b1244843c0532fd6a6d0a3b05b5622d0bcb
Patrik Majer <patrik.majer@atlas.cz>
2018-12-29 20:26:31 +0100

    fix module name for beaker

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,11 +3,19 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 run_puppet_install_helper
+
+RSpec.configure do |c|
+  # Readable test descriptions
+  c.formatter = :documentation
+  c.before :suite do
+    hosts.each do |host|
+      # fix module name
+      on(host, "cd /etc/puppetlabs/code/modules && ln -s sugarcrmstack_v3 sugarcrmstack")
+    end
+  end
+end
+
 install_module
-
-# fix module nam
-File.rename("/etc/puppetlabs/code/modules/sugarcrmstack_v3", "/etc/puppetlabs/code/modules/sugarcrmstack")
-
 install_module_dependencies
 
 RSpec.configure do |c|
