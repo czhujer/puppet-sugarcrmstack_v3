5ac25356dba3da39b077c05523d0490b67342144
Patrik Majer <patrik.majer@atlas.cz>
2018-12-29 20:45:49 +0100

    debug module status for acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,12 +4,18 @@ require 'beaker/module_install_helper'
 
 run_puppet_install_helper
 
+# fix module name
 hosts.each do |host|
-  # fix module name
   on(host, "cd /etc/puppetlabs/code/modules && ln -s sugarcrmstack_v3 sugarcrmstack")
 end
 
 install_module
+
+# debug module status
+hosts.each do |host|
+  on(host, "ls -lh /etc/puppetlabs/code/modules/sugarcrmstack/*")
+end
+
 install_module_dependencies
 
 RSpec.configure do |c|
